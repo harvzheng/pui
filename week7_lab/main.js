d8d241301efb8da76ce321f0204c268307a050af
Harvey Zheng <zheng.harvey5@gmail.com>
2022-03-14 10:47:55 -0400

    add week 7 lab

---
+++ week7_lab/main.js
@@ -0,0 +1,56 @@
+function Panda(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = "an icon of a panda";
+  this.image = "images/panda.svg";
+}
+function Goose(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = "an icon of a goose";
+  this.image = "images/goose.svg";
+}
+function Hamster(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = "an icon of a hamster";
+  this.image = "images/hamster.svg";
+}
+
+animal = [new Panda("Po", 6), new Goose("Chucky", 4), new Hamster("Buster", 2)]
+animal_names = ["Joe", "Jackson", "Jarles", "Jamie"]
+
+function generateRandomIndex(maxIndex) {
+  return Math.floor(Math.random() * maxIndex)
+}
+
+function generateRandomName() {
+  return animal_names[generateRandomIndex(animal_names.length)]
+}
+
+function generateRandomAge(maxAge = 10) {
+  return generateRandomIndex(maxAge)
+}
+
+function generateRandomAnimal(maxIndex) {
+  random_animal = animal[generateRandomIndex(animal.length)]
+  if (random_animal instanceof Panda) {
+    return new Panda(generateRandomName(), generateRandomAge())
+  } else if (random_animal instanceof Goose) {
+    return new Goose(generateRandomName(), generateRandomAge())
+    
+  } else if (random_animal instanceof Hamster) {
+    return new Hamster(generateRandomName(), generateRandomAge())
+  }
+}
+
+function onLoad() {
+  var animal = generateRandomAnimal()
+  var text = document.getElementById("animal_text")
+  var title = document.getElementById("animal_name")
+  var img = document.getElementById("animal_image")
+  img.setAttribute("src", animal.image)
+  img.setAttribute("alt", animal.image_alt)
+  title.innerHTML = animal.name
+  text.innerHTML = animal.age
+}
